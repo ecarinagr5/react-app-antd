828b969a87d1766e0b58ec89c4fe560a43c79ac8
carina gonzalez <estela.gonzalezruiz@bedbath.com>
2021-10-25 05:12:07 -0500

    Hooks Redux

--- src/components/Counter_normal.js
+++ src/components/Counter_normal.js
@@ -2,22 +2,11 @@ import React from "react";
 import { Row, Col, Typography, Button, Card} from 'antd';
 
 //import REDUX
-//import { connect } from 'react-redux';
-
-//using Hooks
-import { useDispatch , useSelector} from 'react-redux';
+import { connect } from 'react-redux';
 import {actionTypes} from '../redux';
 
 
-//const Counter = ({counter,decrement,increment}) =>{
-    const Counter = () =>{
-    //HOOKS useSelector, useDispatch
-    const counter = useSelector(state => state.counter);
-    const dispatch = useDispatch();
-
-    //ACTIONS TYPES
-    const increment = () => dispatch({type:actionTypes.INCREMENT});
-    const decrement = () => dispatch({type:actionTypes.DECREMENT});
+const Counter = ({counter,decrement,increment}) =>{
 
     return (
         <React.Fragment>
@@ -40,11 +29,12 @@ import {actionTypes} from '../redux';
     ) 
 }
 
-//const mapStateToProps = state => ({  counter:state.counter})
+const mapStateToProps = state => ({
+    counter:state.counter
+})
 
-/*const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = dispatch => ({
     increment:() => { dispatch({type:actionTypes.INCREMENT}) },
     decrement:() => { dispatch({type:actionTypes.DECREMENT}) },
-})*/
-//export default connect(mapStateToProps,mapDispatchToProps)(Counter);
-export default Counter;
+})
+export default connect(mapStateToProps,mapDispatchToProps)(Counter);
